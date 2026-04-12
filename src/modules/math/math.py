d7008c699afa8d86aa0cbd1bf4c7c4f33bca7c97
Francisco Ossian <ossian.neto@gmail.com>
2026-04-12 17:43:51 -0300

    update math

--- src/modules/math/math.py
+++ src/modules/math/math.py
@@ -1,10 +1,30 @@
 class Matrix:
+    """Matrix class
+    
+    Attributes:
+        rows (int): number of rows
+        cols (int): number of columns
+        data (list): matrix data
+
+    Methods:
+        __matmul__(self, other): matrix multiplication
+        T: transpose matrix
+    """
     def __init__(self, rows, cols, data=None):
         self.rows = rows
         self.cols = cols
         self.data = data or [[0 for _ in range(cols)] for _ in range(rows)]
 
     def __matmul__(self, other):
+        """matrix multiplication. About this overload, see: https://docs.python.org/3/reference/datamodel.html#emulating-numeric-types
+        Args:
+            other (Matrix): other matrix, must have the same number of rows as the number of columns of self
+        Returns:
+            Matrix: result matrix
+        """
+        if self.cols != other.rows:
+            raise ValueError(f'Number of columns of self ({self.cols}) must be equal to the number of rows of other ({other.rows})')
+        
         result = Matrix(self.rows, other.cols)
 
         for i in range(self.rows):
@@ -15,6 +35,10 @@ class Matrix:
     
     @property
     def T(self):
+        """transpose matrix
+        Returns:
+            Matrix: transpose matrix
+        """
         t = [list(t) for t in list(zip(*self.data))]
         return Matrix(len(t), len(t[0]), t)
 
@@ -24,21 +48,67 @@ class Matrix:
 # escalado.T.data
 
 def get_identity_matrix(n=3):
+    """identity matrix
+    Args:
+        n (int): size of the matrix
+    Returns:
+        Matrix: identity matrix
+    """
     return Matrix(n, n, [[1 if i == j else 0 for j in range(n)] for i in range(n)])
 
 def get_translation_matrix(tx, ty):
+    """translation matrix
+    Args:
+        tx (float): translation in x
+        ty (float): translation in y
+    Returns:
+        Matrix: translation matrix
+    """
     return Matrix(3, 3, [[1, 0, tx], [0, 1, ty], [0, 0, 1]])
 
 def get_rotation_matrix(angle):
+    """rotation matrix
+
+    Args:
+        angle (float): angle in radians
+
+    Returns:
+        Matrix: rotation matrix
+    """
     return Matrix(3, 3, [[cos(angle), -sin(angle), 0], [sin(angle), cos(angle), 0], [0, 0, 1]])
 
 def get_scale_matrix(sx, sy):
+    """scale matrix
+
+    Args:
+        sx (float): scale factor in x
+        sy (float): scale factor in y
+
+    Returns:
+        Matrix: scale matrix
+    """
     return Matrix(3, 3, [[sx, 0, 0], [0, sy, 0], [0, 0, 1]])
 
 def get_shear_matrix(sx, sy):
+    """shear matrix
+
+    Args:
+        sx (float): shear factor in x
+        sy (float): shear factor in y
+
+    Returns:
+        Matrix: shear matrix
+    """
     return Matrix(3, 3, [[1, sx, 0], [sy, 1, 0], [0, 0, 1]])
 
 def get_reflection_matrix(axis):
+    """reflection matrix
+    Args:
+        axis (str): axis of reflection
+
+    Returns:
+        Matrix: reflection matrix
+    """
     if axis == 'x':
         return Matrix(3, 3, [[1, 0, 0], [0, -1, 0], [0, 0, 1]])
     elif axis == 'y':
